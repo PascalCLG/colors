f65f7d485c8100a73f5977454c96371807817d62
Pascal Remillard <pascal.remillard@clg.qc.ca>
2023-11-09 11:17:58 -0500

    Changement titre colorListe1

--- colorsList1.php
+++ colorsList1.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Listes de couleurs (version #2)</title>
+    <title>Listes de couleurs (V3)</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/colors.css">
 </head>
